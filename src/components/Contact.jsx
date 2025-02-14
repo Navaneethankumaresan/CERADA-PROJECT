f7c8b756c4527e998bfe8d6bcbabfbe6d835e6cf
Navaneethankumaresan <navaneethankumaresan32@gmail.com>
2025-02-14 14:53:50 +0530

    footer update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="container mx-auto p-8">
+    <div className="container mx-auto p-52">
       <h2 className="text-3xl font-bold text-blue-900 mb-6">Contact Us</h2>
       <p className="text-lg text-gray-700">Email: info@icsteet2026.com</p>
       <p className="text-lg text-gray-700">Phone: +123 456 7890</p>
+     
     </div>
+    
   );
 };
 
--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Theme = () => {
   return (
-    <div className="container mx-auto p-8">
+    <div className="container mx-auto p-52">
       <h2 className="text-3xl font-bold text-blue-900 mb-6">Conference Theme</h2>
       <p className="text-lg text-gray-700">
         "Bridging the Gap: Innovations and Challenges in Social Sciences, Teaching & Education, Engineering and Technology."
